95319a0e5df03809af23540b3959389c4e47de0a
Edmil Jampier Saire Bustamante <174449@unsaac.edu.pe>
2026-06-12 19:31:17 -0500

    fix: restore TikZ dependencies and missing bibliography entries for Marco Teorico

---
+++ Proyecto Segunda Parte/Informe/merge.ps1
@@ -0,0 +1,12 @@
+﻿$anteproyecto = 'D:\Proyectos\Cuantica\Proyecto Semestral\Anteproyecto\main.tex'
+$cap2 = 'd:\Proyectos\Cuantica\Proyecto Segunda Parte\Informe\contenido\cap_2_marco_teorico.tex'
+
+$lines_ante = Get-Content $anteproyecto -Encoding UTF8
+$teoria = $lines_ante[253..505]
+
+$lines_cap2 = Get-Content $cap2 -Encoding UTF8
+$header = $lines_cap2[0..4]
+$footer = $lines_cap2[5..($lines_cap2.Length - 1)]
+
+$new_content = $header + $teoria + $footer
+$new_content | Set-Content $cap2 -Encoding UTF8
